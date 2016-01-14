574d757f1306334f39fff1b5ba83fe78866aecf2
Pedro Santos <pedro@codurance.com>
2016-01-14 10:40:22 +0000

    First commit

---
+++ GitHubAnimator/Program.fs
@@ -0,0 +1,40 @@
+﻿namespace GitHubAnimator
+
+module program =
+
+    open Animator
+
+    type CommandLineOption = 
+                            {
+                                RepositoryOwner:string;
+                                RepositoryName:string;
+                                File:string;
+                                RevealTemplatePath:string;
+                                OutputPath:string;
+                            }
+
+    [<EntryPoint>]
+    let main argv = 
+
+        let commandLineOptions = { 
+                                    RepositoryOwner = "pedromsantos"; 
+                                    RepositoryName = "FSharpKatas"; 
+                                    File="Bowling.fs";
+                                    RevealTemplatePath = ".\\reveal.js";
+                                    OutputPath = ".\\Presentation"
+                                }
+
+        let parameters = {
+                            Owner = commandLineOptions.RepositoryOwner;
+                            Repository = commandLineOptions.RepositoryName;
+                            File =  commandLineOptions.File;
+                            }
+
+
+        createClient
+            |> createPresentation parameters
+            |> savePresentation 
+                commandLineOptions.RevealTemplatePath 
+                commandLineOptions.OutputPath
+
+        0
---
+++ GitHubAnimator/Scripts/load-references-debug.fsx
@@ -0,0 +1,10 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../packages/Octokit.0.17.0/lib/net45/Octokit.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Net.Http.dll"
+#r "System.Numerics.dll"
+#r "../packages/NUnit.2.6.4/lib/nunit.framework.dll"
+#r "../packages/FsUnit.1.4.1.0/lib/net45/FsUnit.NUnit.dll"
+#r "../packages/FsUnit.1.4.1.0/lib/net45/NHamcrest.dll"
